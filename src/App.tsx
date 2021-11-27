7b05999986d42e6bcea9fe8d6f6a642b816348ea
NikolaGanchev <nikolamihailovg@gmail.com>
2021-11-27 21:21:10 +0200

    Minor bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -20,314 +20,317 @@ import registerAll from './utils/UnitImports';
 import { TagsContext } from './components/TagsContext';
 
 const StyledCarousel = styled(CardCarousel)`
-  position: relative;
-  width: 100%;
-  height: 85vh;
-  transition: all 1s ease;
-  overflow: visible;
-  @media (max-width: 470px) {
-    display: flex;
-    flex-wrap: wrap;
-    align-content: flex-end;
-  }
+    position: relative;
+    width: 100%;
+    height: 85vh;
+    transition: all 1s ease;
+    overflow: visible;
+    @media (max-width: 470px) {
+        display: flex;
+        flex-wrap: wrap;
+        align-content: flex-end;
+    }
 `
 const Top = styled.div`
-  width: 100%;
-  height: 3rem;
-  position: relative;
-  transition: all 1s ease;
-  display: flex;
-  background-color: ${props => props.theme.transparent};
+    width: 100%;
+    height: 3rem;
+    position: relative;
+    transition: all 1s ease;
+    display: flex;
+    background-color: ${props => props.theme.transparent};
 `
 
 const animation = keyframes`
-  0% {
-        transform: rotate(0deg);
-    }
+    0% {
+                transform: rotate(0deg);
+        }
 
-    100% {
-        transform: rotate(360deg);
-    }
+        100% {
+                transform: rotate(360deg);
+        }
 `
 
 const StyledSettings = styled(Settings)`
-  width: 3rem;
-  height: 3rem;
-  fill: ${props => props.theme.secondary};
-  cursor: pointer;
-  transition: all 1s ease;
-  z-index: 400;
+    width: 3rem;
+    height: 3rem;
+    fill: ${props => props.theme.secondary};
+    cursor: pointer;
+    transition: all 1s ease;
+    z-index: 400;
 `
 
 const StyledButton = styled.button`
-    width: 4rem;
-    height: 4rem;
-    border: none;
-    background-color: ${props => props.theme.transparent};
-    z-index: 500;
-    position: relative;
-    margin-left: auto;
-    margin-top: 1rem;
-    margin-right: 1rem;
-    display: inline-flex;
-    & svg:hover {
-      box-sizing: content-box;
-      transform-origin: center;
-      transform-box: fill-box;
-      animation: ${animation} 5s infinite forwards linear;
-      transition: all 1s ease;
-    }
+        width: 4rem;
+        height: 4rem;
+        border: none;
+        background-color: ${props => props.theme.transparent};
+        z-index: 500;
+        position: relative;
+        margin-left: auto;
+        margin-top: 1rem;
+        margin-right: 1rem;
+        display: inline-flex;
+        & svg:hover {
+            box-sizing: content-box;
+            transform-origin: center;
+            transform-box: fill-box;
+            animation: ${animation} 5s infinite forwards linear;
+            transition: all 1s ease;
+        }
 `
 
 const Footer = styled.div`
-  width: 100%;
-  height: 3rem;
-  text-align: center;
-  margin: 0;
-  padding: 0;
-  transition: all 1s ease;
-  font-size: 1.2rem;
-  @media (max-width: 470px) {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+    width: 100%;
+    height: 3rem;
+    text-align: center;
+    margin: 0;
+    padding: 0;
+    transition: all 1s ease;
+    font-size: 1.2rem;
+    @media (max-width: 470px) {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 `
 const activeIndexKey = "activeIndex";
 
 function App() {
-  const [currentTheme, setTheme] = useState(theme);
-  const [t] = useTranslation("common");
-  const [tt] = useTranslation("tags");
-  const [showLoader, setShowLoader] = useState(true);
-  const [cards, setCards] = useState<StudyUnit[] | null>(null);
-  const [currentStudyUnit, setCurrentStudyUnit] = useState<StudyUnit | null>(null);
-  const [settingsOpen, setSettingsOpen] = useState(false);
-  const { unitId }: any = useParams();
-  const [showIndexedDBNotSupportedWarning, setShowIndexedDBNotSupportedWarning] = useState(false);
-  const tags = useContext(TagsContext);
-  const history = useHistory();
+    const [currentTheme, setTheme] = useState(theme);
+    const [t] = useTranslation("common");
+    const [tt] = useTranslation("tags");
+    const [showLoader, setShowLoader] = useState(true);
+    const [cards, setCards] = useState<StudyUnit[] | null>(null);
+    const [currentStudyUnit, setCurrentStudyUnit] = useState<StudyUnit | null>(null);
+    const [settingsOpen, setSettingsOpen] = useState(false);
+    const { unitId }: any = useParams();
+    const [showIndexedDBNotSupportedWarning, setShowIndexedDBNotSupportedWarning] = useState(false);
+    const tags = useContext(TagsContext);
+    const history = useHistory();
 
-  useEffect(() => {
-    document.title = t("app.name");
-  }, [t]);
+    useEffect(() => {
+        document.title = t("app.name");
+    }, [t]);
 
-  const hide = () => {
-    setShowLoader(false);
-  }
-
-  const endUnit = () => {
-    setCurrentStudyUnit(null);
-    setTimeout(() => {
-      changeToNext(true);
-    }, 500);
-  }
-
-  get("theme").then((val: string) => {
-    if (val === "dark") {
-      setTheme(darkTheme);
-    }
-    else {
-      setTheme(theme);
-    }
-  });
-
-  const changeTheme = (isDark: boolean) => {
-    if (isDark) {
-      setTheme(darkTheme);
-      set("theme", themes.darkTheme);
-    }
-    else {
-      setTheme(theme);
-      set("theme", themes.theme);
-    }
-  }
-
-  const switchToUrlUnitIfPossible = (unitId: string, cards: StudyUnit[]) => {
-    if (!unitId || unitId.trim() === "") {
-      return;
+    const hide = () => {
+        setShowLoader(false);
     }
 
-    const normalized = unitId.trim().toLowerCase();
-
-    for (let i = 0; i < cards.length; i++) {
-      if (cards[i].id.toLowerCase() === normalized) {
-        setActiveIndex(i);
-        set(activeIndexKey, i);
-        break;
-      }
-    }
-  }
-
-  // Carousel controller
-  const [opacity, setOpacity] = useState(100);
-  const [transX, setTransX] = useState(0);
-  const [isTransition, setIsTransition] = useState(true);
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const loadActiveIndexFromStorage = async () => {
-    let val: number | undefined = await get(activeIndexKey);
-    if (val) {
-      setActiveIndex(val);
-    }
-  }
-
-  const changeToNext = (unlockNext = false) => {
-    changeToArbitrary(activeIndex + 1, unlockNext);
-  }
-
-  const changeToPrevious = () => {
-    changeToArbitrary(activeIndex - 1);
-  }
-
-  const changeToArbitrary = (newActiveIndex: number, unlockNew = false, callback = () => { }) => {
-    if (cards === null) {
-      return;
-    }
-    if (newActiveIndex < 0 || newActiveIndex > cards.length - 1) {
-      return;
-    }
-    const transitionToNew = (newActiveIndex > activeIndex) ? 300 : -300;
-    setTransX(-transitionToNew);
-    setOpacity(0);
-
-    setTimeout(() => {
-
-      requestAnimationFrame(() => {
-
-        setIsTransition(false);
-        setTransX(transitionToNew);
-        setActiveIndex(newActiveIndex);
-        set(activeIndexKey, newActiveIndex);
-        history.replace(cards[newActiveIndex].id);
-
-        requestAnimationFrame(() => {
-
-          setIsTransition(true);
-          setOpacity(100);
-          setTransX(0);
-
-          if (unlockNew && cards !== null) {
-
-            setTimeout(() => {
-
-              requestAnimationFrame(() => {
-                let copy: any = [];
-                Object.assign(copy, cards);
-                copy[newActiveIndex].unlocked = true;
-                setCards(copy);
-                changeStudyUnit(cards[newActiveIndex]);
-              })
-            }, animationLength * 1000)
-
-          }
-
-          callback();
-        })
-      })
-    }, animationLength * 1000);
-  }
-
-  const onSelect = (id: string) => {
-    if (cards == null) {
-      return;
+    const endUnit = () => {
+        setCurrentStudyUnit(null);
+        setTimeout(() => {
+            changeToNext(true);
+        }, 500);
     }
 
-    for (let i = 0; i < cards.length; i++) {
-      if (id === cards[i].id) {
-        changeToArbitrary(i);
-        break;
-      }
+    get("theme").then((val: string) => {
+        if (val === "dark") {
+            setTheme(darkTheme);
+        }
+        else {
+            setTheme(theme);
+        }
+    });
+
+    const changeTheme = (isDark: boolean) => {
+        if (isDark) {
+            setTheme(darkTheme);
+            set("theme", themes.darkTheme);
+        }
+        else {
+            setTheme(theme);
+            set("theme", themes.theme);
+        }
     }
-  }
 
-  const job = async () => {
-
-    const func = async (success: boolean) => {
-      const onError = () => {
-        setShowIndexedDBNotSupportedWarning(true);
-        generateAndGetStudyUnits((units: StudyUnit[]) => {
-          onLoad(units);
-        });
-      }
-
-      const onLoad = async (units: StudyUnit[]) => {
-        tags.current.load(getTags(units));
-        setCards(units);
-        await loadActiveIndexFromStorage();
-        switchToUrlUnitIfPossible(unitId, units);
-        registerAll();
-      }
-
-      const getTags = (units: StudyUnit[]) => {
-        const tags: TagSet[] = [];
-        for (let unit of units) {
-          tags.push(new TagSet(unit, tt(`tags.${unit.id}`, { returnObjects: true })))
+    const switchToUrlUnitIfPossible = (unitId: string, cards: StudyUnit[]) => {
+        if (!unitId || unitId.trim() === "") {
+            return;
         }
 
-        return tags;
-      }
+        const normalized = unitId.trim().toLowerCase();
 
-      if (success) {
-        const callback = async (units: StudyUnit[] | null) => {
-          if (units == null) {
-            await onError();
-          }
-          else {
-            await onLoad(units);
-          }
-        };
-        await getAllStudyUnitsArray(callback);
-      }
-      else {
-        onError();
-      }
+        for (let i = 0; i < cards.length; i++) {
+            if (cards[i].id.toLowerCase() === normalized) {
+                setActiveIndex(i);
+                set(activeIndexKey, i);
+                break;
+            }
+        }
     }
 
-    await generateStudyUnitsIfNeeded(func);
-  }
+    // Carousel controller
+    const [opacity, setOpacity] = useState(100);
+    const [transX, setTransX] = useState(0);
+    const [isTransition, setIsTransition] = useState(true);
+    const [activeIndex, setActiveIndex] = useState(0);
 
-  return (
-    <ThemeProvider theme={currentTheme}>
-      <GlobalStyle />
-      <div>
-        <Alert
-          heading={t("app.warning")}
-          warning={t("app.indexedDBNotSupportedWarning")}
-          ok={t("app.ok")}
-          hide={() => { setShowIndexedDBNotSupportedWarning(false) }}
-          isShowing={showIndexedDBNotSupportedWarning} />
-        <SettingsDisplay theme={currentTheme === darkTheme ? themes.darkTheme : themes.theme}
-          changeTheme={changeTheme} close={() => { setSettingsOpen(false) }} isShowing={settingsOpen} />
-        <Loader title={t("app.name")} motto={t("app.motto")} hide={hide} job={job} isShowing={showLoader} />
-        {(currentStudyUnit) ?
-          (<Unit unit={currentStudyUnit} back={() => { setCurrentStudyUnit(null) }} endUnit={endUnit} />) :
-          (<div>
-            <Top>
-              <SearchBar
-                cards={cards}
-                changeToArbitrary={changeToArbitrary}
-                onSelect={onSelect} />
-              <StyledButton onClick={() => { setSettingsOpen(true) }} aria-label={t("app.settings")}>
-                <StyledSettings>
-                </StyledSettings>
-              </StyledButton>
-            </Top>
-            <StyledCarousel
-              cards={cards}
-              setStudyUnit={setCurrentStudyUnit}
-              activeIndex={activeIndex}
-              changeToPrevious={changeToPrevious}
-              changeToNext={changeToNext}
-              transX={transX}
-              isTransition={isTransition}
-              opacity={opacity} />
-            <Footer>{t("app.motto")}</Footer>
-          </div>)
+    const loadActiveIndexFromStorage = async () => {
+        let val: number | undefined = await get(activeIndexKey);
+        if (val) {
+            setActiveIndex(val);
         }
-      </div>
-    </ThemeProvider>
-  );
+    }
+
+    const changeToNext = (unlockNext = false) => {
+        changeToArbitrary(activeIndex + 1, unlockNext);
+    }
+
+    const changeToPrevious = () => {
+        changeToArbitrary(activeIndex - 1);
+    }
+
+    const changeToArbitrary = (newActiveIndex: number, unlockNew = false, callback = () => { }) => {
+        if (cards === null) {
+            return;
+        }
+        if (newActiveIndex < 0 || newActiveIndex > cards.length - 1) {
+            return;
+        }
+        const transitionToNew = (newActiveIndex > activeIndex) ? 300 : -300;
+        setTransX(-transitionToNew);
+        setOpacity(0);
+
+        setTimeout(() => {
+
+            requestAnimationFrame(() => {
+
+                setIsTransition(false);
+                setTransX(transitionToNew);
+                setActiveIndex(newActiveIndex);
+                set(activeIndexKey, newActiveIndex);
+                history.replace(cards[newActiveIndex].id);
+
+                requestAnimationFrame(() => {
+
+                    setIsTransition(true);
+                    setOpacity(100);
+                    setTransX(0);
+
+                    if (unlockNew && cards !== null) {
+
+                        setTimeout(() => {
+
+                            requestAnimationFrame(() => {
+                                let copy: any = [];
+                                Object.assign(copy, cards);
+                                copy[newActiveIndex].unlocked = true;
+                                setCards(copy);
+                                changeStudyUnit(cards[newActiveIndex]);
+                            })
+                        }, animationLength * 1000)
+
+                    }
+
+                    callback();
+                })
+            })
+        }, animationLength * 1000);
+    }
+
+    const onSelect = (id: string) => {
+        if (cards == null) {
+            return;
+        }
+
+        for (let i = 0; i < cards.length; i++) {
+            if (id === cards[i].id) {
+				if (id === cards[activeIndex].id) {
+					break;
+				}
+                changeToArbitrary(i);
+                break;
+            }
+        }
+    }
+
+    const job = async () => {
+
+        const func = async (success: boolean) => {
+            const onError = () => {
+                setShowIndexedDBNotSupportedWarning(true);
+                generateAndGetStudyUnits((units: StudyUnit[]) => {
+                    onLoad(units);
+                });
+            }
+
+            const onLoad = async (units: StudyUnit[]) => {
+                tags.current.load(getTags(units));
+                setCards(units);
+                await loadActiveIndexFromStorage();
+                switchToUrlUnitIfPossible(unitId, units);
+                registerAll();
+            }
+
+            const getTags = (units: StudyUnit[]) => {
+                const tags: TagSet[] = [];
+                for (let unit of units) {
+                    tags.push(new TagSet(unit, tt(`tags.${unit.id}`, { returnObjects: true })))
+                }
+
+                return tags;
+            }
+
+            if (success) {
+                const callback = async (units: StudyUnit[] | null) => {
+                    if (units == null) {
+                        await onError();
+                    }
+                    else {
+                        await onLoad(units);
+                    }
+                };
+                await getAllStudyUnitsArray(callback);
+            }
+            else {
+                onError();
+            }
+        }
+
+        await generateStudyUnitsIfNeeded(func);
+    }
+
+    return (
+        <ThemeProvider theme={currentTheme}>
+            <GlobalStyle />
+            <div>
+                <Alert
+                    heading={t("app.warning")}
+                    warning={t("app.indexedDBNotSupportedWarning")}
+                    ok={t("app.ok")}
+                    hide={() => { setShowIndexedDBNotSupportedWarning(false) }}
+                    isShowing={showIndexedDBNotSupportedWarning} />
+                <SettingsDisplay theme={currentTheme === darkTheme ? themes.darkTheme : themes.theme}
+                    changeTheme={changeTheme} close={() => { setSettingsOpen(false) }} isShowing={settingsOpen} />
+                <Loader title={t("app.name")} motto={t("app.motto")} hide={hide} job={job} isShowing={showLoader} />
+                {(currentStudyUnit) ?
+                    (<Unit unit={currentStudyUnit} back={() => { setCurrentStudyUnit(null) }} endUnit={endUnit} />) :
+                    (<div>
+                        <Top>
+                            <SearchBar
+                                cards={cards}
+                                changeToArbitrary={changeToArbitrary}
+                                onSelect={onSelect} />
+                            <StyledButton onClick={() => { setSettingsOpen(true) }} aria-label={t("app.settings")}>
+                                <StyledSettings>
+                                </StyledSettings>
+                            </StyledButton>
+                        </Top>
+                        <StyledCarousel
+                            cards={cards}
+                            setStudyUnit={setCurrentStudyUnit}
+                            activeIndex={activeIndex}
+                            changeToPrevious={changeToPrevious}
+                            changeToNext={changeToNext}
+                            transX={transX}
+                            isTransition={isTransition}
+                            opacity={opacity} />
+                        <Footer>{t("app.motto")}</Footer>
+                    </div>)
+                }
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
